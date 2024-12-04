fb233cadd641d8bab9814cb6a98d00c4049f5c26
railroad12 <unaware.chess974@passmail.com>
2024-12-04 22:17:43 +0530

    added file for vid2

---
+++ youtube_netninja_pythoncrashcourse/vid2_pythonbasics.py
@@ -0,0 +1,15 @@
+#datatypes
+
+print("sting")
+print(123)
+print(1.23)
+
+#operators
+print(10+2)
+print(10-2)
+print(10*2)
+print(10/2)
+
+#variable
+input1 = input("enter name: ")
+print("Hello "+input1+", have a nice day")
